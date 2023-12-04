1de85af966c2edefc97cbfb183e7a1ced7930a9e
Hritik Shashi Arora <144352807+A00477939@users.noreply.github.com>
2023-12-04 11:42:15 -0800

    Add User Model with validation

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace EMS_App.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "Name Is Required")]
+        [RegularExpression(@"^[^\d]+$", ErrorMessage = "Name cannot contain numbers")]
+        public string Uname { get; set; }
+        [Required(ErrorMessage = "Date of Birth Is Required")]
+        public DateTime Udob { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Uemail { get; set; }
+        [Required(ErrorMessage = "Canadian or USA Mobile Number is required")]
+        [RegularExpression(@"^(\+?1\s?[-.\s]?)?(\(\d{3}\)|\d{3})[-.\s]?\d{3}[-.\s]?\d{4}$", ErrorMessage = "Invalid Mobile Number")]
+        public int Unumber { get; set; }
+
+
+
+
+    }
+}
