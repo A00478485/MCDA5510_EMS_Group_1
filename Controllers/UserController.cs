05a6bcb78286dcb0b9691c711d42053471252c66
Hritik Shashi Arora <144352807+A00477939@users.noreply.github.com>
2023-12-04 14:23:16 -0800

    Added Register View and bootstrap css

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMS_App.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EMS_App.Data
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    
+        
+    }
+}
