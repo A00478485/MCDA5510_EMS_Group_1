c6cd5440501d71e4b8fb38930789ce55a5e0fd40
Hritik Shashi Arora <144352807+A00477939@users.noreply.github.com>
2023-12-04 10:23:21 -0800

    Added Global Setting

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace EMS_App
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            dotapp.Run();
+            app.Run();
         }
     }
 }
