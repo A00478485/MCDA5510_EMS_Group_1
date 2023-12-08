f17ec96ec0c792ce79c39fc7ba62c2a8f9feba63
Bhavy Doshi <doshibhavy21@gmail.com>
2023-12-07 20:53:53 -0400

    Event Model Added

---
+++ Models/Event.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS_App.Models
+{
+    public class Event
+    {
+        public Guid eventID { get; set; }
+
+        [Required]
+        public string eventHeader { get; set; }
+
+        [Required]
+        public string eventDescription { get; set; }
+
+        [Required]
+        public DateTime eventStartTime { get; set; }
+
+        [Required]
+        public DateTime eventEndTime { get; set; }
+ 
+    }
+}
