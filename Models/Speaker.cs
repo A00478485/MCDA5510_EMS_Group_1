cee7b539f6f5d49d92a4d6bae16f613f48b50753
Hritik Shashi Arora <144352807+A00477939@users.noreply.github.com>
2023-12-08 14:34:17 -0800

    Squashed commit of the following:
    commit b62e7e4dcfde6b5a6d55e9c811b8a3546b872454
    Author: Hritik Shashi Arora <144352807+A00477939@users.noreply.github.com>
    Date:   Fri Dec 8 12:24:32 2023 -0800
    
        Bug fixing
    
        Fix bug causing incorrect user  Register
    
    commit 29e803a5d5817eb6da2b8659116ff0494505944e
    Author: Hritik Shashi Arora <144352807+A00477939@users.noreply.github.com>
    Date:   Fri Dec 8 10:39:40 2023 -0800
    
        Minor bug fix
    
    commit 9bf309de0f5a33c2ac581ef8670fa41075b5626b
    Author: Hritik Shashi Arora <144352807+A00477939@users.noreply.github.com>
    Date:   Fri Dec 8 10:03:33 2023 -0800
    
        Refine CSS styling
    
    commit 4d0179b583433139b94c985e491dad9ded461d96
    Author: Hritik Shashi Arora <144352807+A00477939@users.noreply.github.com>
    Date:   Fri Dec 8 01:13:46 2023 -0800
    
        Completed login/logout functionality and added Event and Speaker models
    
        - Implemented complete login and logout functionality
        - Added Event model to manage events in the system
        - Added Speaker model to handle speaker-related information
    
    commit 348714e46346eb6b20703d2c31ecf7c0d51c2f1d
    Author: Hritik Shashi Arora <144352807+A00477939@users.noreply.github.com>
    Date:   Wed Dec 6 18:28:01 2023 -0800
    
        Bug Fixing and added Speaker Models

---
+++ Models/Speaker.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EMS_App.Models
+{
+    public class Speaker
+    {
+        [Key]
+        public int Sid { get; set; }
+        [Required]
+        public string Sname { get; set; }
+        [Required]
+        public string SBio { get; set; }
+        [Required]
+        public string Simage { get; set; }
+
+    }
+}
