bbfd7c861db84737a2fda246872afcd7fdb07e2f
Bhavy Doshi <doshibhavy21@gmail.com>
2023-12-07 23:46:08 -0400

    Event timeline page added

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMS_App.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
