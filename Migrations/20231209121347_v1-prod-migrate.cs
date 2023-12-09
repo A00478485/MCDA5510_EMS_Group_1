eca503c7d2f51456ec405dc12990f1c23b3cea48
Bhavy Doshi <doshibhavy21@gmail.com>
2023-12-09 11:53:02 -0400

    Amending Migration

---
+++ Migrations/20231209121347_v1-prod-migrate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMS_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1prodmigrate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
