b051bd8b5f9ab7284796ae49f71bc813d4de111e
Bhavy Doshi <doshibhavy21@gmail.com>
2023-12-09 01:33:56 -0400

    Changing DBContext for Design

---
+++ Data/EMSDesignDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EMS_App.Data
+{
+    public class EMSDesignDbContext : IDesignTimeDbContextFactory<EMSContext>
+    {
+        public EMSContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EMSContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EcommerceDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new EMSContext(optionsBuilder.Options);
+        }
+    }
+}
